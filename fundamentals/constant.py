beb0c0b9ebb10f81eed6b4abed1d08b8f0c71725
Don Ky <don.d.ky@gmail.com>
2019-12-08 09:57:26 -0500

    Fix constant time

--- fundamentals/constant.py
+++ fundamentals/constant.py
@@ -2,16 +2,14 @@ from stopwatch import Stopwatch
 # now matter how large n gets the runtime of our algorithm is constant.
 
 def constant_run_time(N):
-    #run 50 times and then return True
     total_sum = 0
     for i in range(0, 50):
         total_sum += i
     return total_sum
 
 if __name__ == "__main__":
-    input_sizes = [100, 200, 400, 800, 1600, 3200, 6400, 320000, 640000,
-            1280000]
+    input_sizes = [100, 200, 400, 800, 1600, 3200, 6400]
     for input_size in input_sizes:
-        time = Stopwatch()
+        timer = Stopwatch()
         constant_run_time(input_size)
         print(input_size, timer.elapsed_time())
