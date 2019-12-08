fe71babd4fdb91a6cb6e53d3b8a4d66d7a848f86
Don Ky <don.d.ky@gmail.com>
2019-12-08 13:51:31 -0500

    Add notes to constant time

--- fundamentals/constant.py
+++ fundamentals/constant.py
@@ -4,12 +4,16 @@ from stopwatch import Stopwatch
 def constant_run_time(N):
     total_sum = 0
     for i in range(0, 50):
+        #add all numbers from 0 to 50
         total_sum += i
     return total_sum
 
 if __name__ == "__main__":
     input_sizes = [100, 200, 400, 800, 1600, 3200, 6400]
     for input_size in input_sizes:
+        #This times the function
         timer = Stopwatch()
         constant_run_time(input_size)
+        #what's odd with this output is the run times actually get faster, or
+        #go down but they aren't increasing so that's good?
         print(input_size, timer.elapsed_time())
