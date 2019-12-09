e3c3a555aa6eddb66cca30ddd2426b647a63e87d
Don Ky <don.d.ky@gmail.com>
2019-12-09 09:33:37 -0500

    Add copy.py

---
+++ pythonCrashCourse/copy.py
@@ -0,0 +1,14 @@
+def makeCopy(list):
+    new_list = []
+    _makeCopy(list, new_list, 0)
+    return new_list
+
+def _makeCopy(original_list, new_list, current_index):
+    if current_index == len(original_list):
+        return
+
+    # adding element at index current_index to new list
+    new_list.append(original_list[current_index])
+
+    # moving current_index to next element in original list
+    _makeCopy(original_list, new_list, current_index + 1)
