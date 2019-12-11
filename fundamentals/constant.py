a78ce89e63055b62615abd8bcb769911296e4c7a
Don Ky <don.d.ky@gmail.com>
2019-12-10 21:46:40 -0500

    Clean up constant, update summary

--- fundamentals/constant.py
+++ fundamentals/constant.py
@@ -1,19 +1,22 @@
 from stopwatch import Stopwatch
 # no matter how large n gets the runtime of our algorithm is constant.
 
+
 def constant_run_time(N):
     total_sum = 0
     for i in range(0, 50):
-        #add all numbers from 0 to 50
+        # Add all numbers from 0 to 50
         total_sum += i
+        print(total_sum)
     return total_sum
+
 
 if __name__ == "__main__":
     input_sizes = [100, 200, 400, 800, 1600, 3200, 6400]
     for input_size in input_sizes:
-        #This times the function
+        # This times the function
         timer = Stopwatch()
         constant_run_time(input_size)
-        #what's odd with this output is the run times actually get faster, or
-        #go down but they aren't increasing so that's good?
+        # what's odd with this output is the run times actually get faster, or
+        # go down but they aren't increasing so that's good?
         print(input_size, timer.elapsed_time())
--- fundamentals/summary.md
+++ fundamentals/summary.md
@@ -1,4 +1,7 @@
-# Constant O(1)
+# Constant O(1) - 6:30
 - Constant function does not mean it takes 1 operation.
 - It means it takes on the order of 1
 - Any number of operations as long as it's fixed.
+
+# Logarithmic O(log n) - 9:34
+- As you double N, the runtime will increase by 1.
