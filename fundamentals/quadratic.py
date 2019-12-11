cc0aa5a3dd8c5d75f051d284828668949220f11b
Don Ky <don.d.ky@gmail.com>
2019-12-10 21:49:59 -0500

    Fmt quadratic

--- fundamentals/quadratic.py
+++ fundamentals/quadratic.py
@@ -2,6 +2,7 @@ from stopwatch import Stopwatch
 
 # Quadratic: O(N^2)
 
+
 def quadratic_run_time(N):
     i = 1
     total_operations = 0
@@ -13,8 +14,9 @@ def quadratic_run_time(N):
         i += 1
     return total_operations
 
+
 if __name__ == "__main__":
-    # input_sizes = [10000, 20000, 40000, 80000, 160000, 320000, 640000, 1280000]
+    # input_sizes = [10000, 20000, 40000, 80000, 160000, 320000, 640000]
     # input_sizes take really long to compute, dropping a zero to make this run
     # slightly quicker.
     input_sizes = [1000, 2000, 4000, 8000, 16000]
