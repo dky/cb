b3152515d36af1370c2287226c88bd8a3f1d949c
Don Ky <don.d.ky@gmail.com>
2019-12-10 21:47:10 -0500

    Space out expontential

--- fundamentals/exponential.py
+++ fundamentals/exponential.py
@@ -1,5 +1,6 @@
 from stopwatch import Stopwatch
 
+
 # Exponential: O(2^N)
 def expoential_run_time(N):
     if N == 0:
@@ -8,6 +9,7 @@ def expoential_run_time(N):
     total_operations += expoential_run_time(N-1)
     total_operations += expoential_run_time(N-1)
     return total_operations
+
 
 if __name__ == "__main__":
     input_sizes = [12, 13, 14, 15, 16, 17]
