70ee4a899f311081a4b957a56469f3c1edcf9b32
Don Ky <don.d.ky@gmail.com>
2019-12-10 21:49:03 -0500

    Log fmt

--- fundamentals/logarithmic.py
+++ fundamentals/logarithmic.py
@@ -1,6 +1,8 @@
 from stopwatch import Stopwatch
 
 # Logarithmic O(log n)
+
+
 def log_run_time(N):
     i = 1
     total_operations = 0
@@ -9,10 +11,11 @@ def log_run_time(N):
         i = i * 2
     return total_operations
 
+
 if __name__ == "__main__":
     input_sizes = [10000, 20000, 40000, 80000, 160000, 320000, 640000, 1280000]
     for input_size in input_sizes:
         timer = Stopwatch()
         total_operations = log_run_time(input_size)
-        #Total operations increases by 1 as we double the input size.
+        # Total operations increases by 1 as we double the input size.
         print(input_size, total_operations, timer.elapsed_time())
